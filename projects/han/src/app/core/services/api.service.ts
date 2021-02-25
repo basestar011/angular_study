459abf4580789ac504f3d698756bcb7f0613fc56
dongjun.han <dongjun.han@owaysoft.com>
2021-02-25 15:42:52 +0900

    환경변수 경로 paths에 추가

--- projects/han/src/app/core/services/api.service.ts
+++ projects/han/src/app/core/services/api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'projects/han/src/environments/environment';
+import { environment } from '@hansenv';
 import { Observable } from 'rxjs';
 
 @Injectable({
